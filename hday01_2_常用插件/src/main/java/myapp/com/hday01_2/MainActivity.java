55998949d8f28e973ee2d5d25ffec05b58b2d7fb
Administrator <839885877@qq.com>
2016-05-30 15:48:20 +0800

    加了一个无用的变量

--- hday01_2_常用插件/src/main/java/myapp/com/hday01_2/MainActivity.java
+++ hday01_2_常用插件/src/main/java/myapp/com/hday01_2/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        int i = 1;
     }
 
     @OnClick({R.id.button, R.id.button2})
